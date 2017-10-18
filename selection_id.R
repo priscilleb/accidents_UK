9ceb8fa4b6bb84100a81c3a400d44a0a82c125b7
charlotte.caucheteux <charlotte.caucheteux@gmail.com>
2017-10-18 16:03:34 +0200

    update plot_ID and selection_ID

--- selection_id.R
+++ selection_id.R
@@ -1,6 +1,6 @@
-selection_id <- function(weather,road,light){
+selection_id <- function(weather,road,light, district){
   return(dataset %>% 
-           filter(Weather_Conditions==weather,Road_Surface_Conditions==road,Light_Conditions==light) %>%
+           filter(Weather_Conditions==weather,Road_Surface_Conditions==road,Light_Conditions==light, Local_Authority_.District.== district) %>%
            pull(Accident_Index))
 }
 

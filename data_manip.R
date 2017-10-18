7d32f3104c5a0a4a1afa9d9457be8f79904e713f
Priscille Boissonnet <PriscilleBoissonnet@MacBook-Pro-de-Priscille.local>
2017-10-18 16:25:52 +0200

    Adding the region/district dataset

--- data_manip.R
+++ data_manip.R
@@ -5,6 +5,15 @@ library(ggplot2)
 dataset <- read.csv("accidents_2012_to_2014.csv")
 
 
-d1 <- dataset %>% select("Accident_Index","Road_Surface_Conditions","Weather_Conditions","Light_Conditions","Day_of_Week")
+d1 <- dataset %>% select("Accident_Index","Road_Surface_Conditions","Weather_Conditions","Light_Conditions","Day_of_Week","Local_Authority_.District.")
 d1$Day_of_Week <- as.factor(d1$Day_of_Week)
 levels(d1$Day_of_Week) <- c("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche")
+d1 <- d1 %>% rename("District"= Local_Authority_.District.)
+
+region <- read.csv("Local_Authority_District_to_Region_December_2016_Lookup_in_England.csv")
+region <- region %>% rename("District" = FID)
+region <- region %>% rename("Region" = RGN16NM)
+region <- region %>% select(District, Region)
+
+d1 <- d1 %>% left_join(region, by="District")
+d1 <- d1 %>% filter(Region != "NA") %>% select(-District)

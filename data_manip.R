ef396cb5da8d4f287f6114e98b9d90274b7d6e92
Priscille Boissonnet <PriscilleBoissonnet@MacBook-Pro-de-Priscille.local>
2017-11-05 10:20:59 +0100

    Mise en place de tests car le nombre de sorties sur la carte ne correspond pas

--- data_manip.R
+++ data_manip.R
@@ -5,7 +5,7 @@ library(ggplot2)
 dataset <- read.csv("accidents_2012_to_2014.csv")
 
 
-d1 <- dataset %>% select("Accident_Index","Road_Surface_Conditions","Weather_Conditions","Light_Conditions","Day_of_Week","Local_Authority_.District.")
+d1 <- dataset %>% select("Accident_Index","Road_Surface_Conditions","Weather_Conditions","Light_Conditions","Day_of_Week","Local_Authority_.District.","Longitude","Latitude")
 d1$Day_of_Week <- as.factor(d1$Day_of_Week)
 levels(d1$Day_of_Week) <- c("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche")
 d1 <- d1 %>% rename("District"= Local_Authority_.District.)
@@ -17,3 +17,5 @@ region <- region %>% select(District, Region)
 
 d1 <- d1 %>% left_join(region, by="District")
 d1 <- d1 %>% filter(Region != "NA") %>% select(-District)
+
+
--- selection_id.R
+++ selection_id.R
@@ -1,9 +1,23 @@
 
-selection_id <- function(weather,road,light, region){
+selection_id <- function(weather,road,light,region){
   return(d1 %>% 
            filter(Weather_Conditions==weather,Road_Surface_Conditions==road,Light_Conditions==light,Region==region) %>%
-           pull(Accident_Index))
+           pull(Accident_Index)
+  )
 }
 
-selection_id("Fine without high winds","Dry","Darkness: Street lights present and lit","North West")
+selection_id("Fine without high winds","Dry","Darkness: Street lights present and lit","North West") 
+
+
+#### TEST ####
+#Quand on View, on voit en bas qu'il y a 3701 lignes
+d1 %>% 
+  filter(Weather_Conditions=="Fine without high winds",Road_Surface_Conditions=="Dry",Light_Conditions=="Darkness: Street lights present and lit",Region=="North West") %>% pull(Accident_Index) %>% 
+  View()
+
+#On retrouve bien ici 3701 lignes
+d1 %>% 
+  filter(Weather_Conditions=="Fine without high winds",Road_Surface_Conditions=="Dry",Light_Conditions=="Darkness: Street lights present and lit",Region=="North West") %>% summarise(count=n())
+
+d1 %>% summarise(count=n()) %>% summary()
 
